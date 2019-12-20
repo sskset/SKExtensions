ad7fb82ba4e108f7d7cf2cbe0e98faeb76f5bcbc
shan ke <shan.ke@zip.co>
2019-12-20 11:40:20 +1100

    init

---
+++ xExtensions.Tests/StringExtensionsTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace xExtensions.Tests
+{
+    public class StringExtensionsTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ xExtensions/EnumerableExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace xExtensions
+{
+    public static class EnumerableExtensions
+    {
+        public static bool IsEmpty<T>(this IEnumerable<T> collection)
+        {
+            return !IsNotEmpty(collection);
+        }
+
+        public static bool IsNotEmpty<T>(this IEnumerable<T> collection)
+        {
+            return collection != null && collection.Any();
+        }
+    }
+}
