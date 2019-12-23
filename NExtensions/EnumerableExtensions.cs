70878cf8aef1649eeff8cd784b0c8879f20e1791
Shan <sskset@gmail.com>
2019-12-23 22:09:18 +1100

    rename projects

---
+++ NExtensions/EnumerableExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+
+namespace NExtensions
+{
+    public static class EnumerableExtensions
+    {
+        public static bool IsNullOrEmpty(this IEnumerable collection)
+        {
+            if(collection == null)
+            {
+                return true;
+            }
+
+            IEnumerator enumerator = collection.GetEnumerator();
+            return enumerator?.Current == null;
+        }
+
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> collection)
+        {
+            return collection == null || !collection.Any();
+        }
+
+        public static bool IsNotNullOrEmptyEmpty<T>(this IEnumerable<T> collection)
+        {
+            return !collection.IsNullOrEmpty();
+        }
+    }
+}
